18f79472c0e06a892d831e9a595b1f2218d8dca1
吉枭雄 <jixiaoxiong@ec.e-chemchina.com>
2019-04-04 21:22:12 +0800

    添加陨石起手

--- keyListener.py
+++ keyListener.py
@@ -4,37 +4,62 @@ from pynput import keyboard
 from loopMain import *
 from stopThread import *
 t = None
+
+#奥元素0-4s开 满勾玉循环-黑人起手
+fullLoopBtn=keyboard.Key.f1
+# 火2s-电1.5s 0勾玉不满-黑人起手
+zeroLoopBtn=keyboard.Key.f2
+# 火3.5-电3开 满勾玉循环-陨石起手起手
+meteorite_fullLoopBtn=keyboard.Key.f3
+
 class keyListener(object):
-    
     def on_press(self,key):
         pass
     def on_release(self,key):
         lm=loopMain();
         st=stopThread();
         global t
-        if key==keyboard.Key.f2:
+        if key==fullLoopBtn:
             if t is not None:
                 try:
                    st.stop_thread(t)
                 except:
+                    print("宏已关闭")
                     print("线程已结束")
                     t = None
                 else:
-                   t = None
+                    print("宏已关闭")
+                    t = None
             else:
                  t = threading.Thread(target=lm.fullLoop, args=(30,))
                  t.start();
-        elif key==keyboard.Key.f4:
+        elif key==zeroLoopBtn:
             if t is not None:
                 try:
                    st.stop_thread(t)
                 except:
+                    print("宏已关闭")
                     print("线程已结束")
                     t = None
                 else:
-                   t = None
+                    print("宏已关闭")
+                    t = None
             else:
                  t = threading.Thread(target=lm.zeroLoop, args=(30,))
+                 t.start();
+        elif key==meteorite_fullLoopBtn:
+            if t is not None:
+                try:
+                   st.stop_thread(t)
+                except:
+                    print("宏已关闭")
+                    print("线程已结束")
+                    t = None
+                else:
+                    print("宏已关闭")
+                    t = None
+            else:
+                 t = threading.Thread(target=lm.meteorite_fullLoop, args=(30,))
                  t.start();
         elif key==keyboard.Key.f12:
             return False
--- loopMain.py
+++ loopMain.py
@@ -2,16 +2,16 @@
 import time
 from pynput import keyboard
 from pykeyboard import PyKeyboard
-from  pynput.mouse import Button, Controller
+from pynput.mouse import Button, Controller
 mouse = Controller()
 k = PyKeyboard()
-#常量定义
-blackMan='1'  #黑人 键盘1
-meteorite='2' #陨石 键盘2
-yuanlibo='3'  #原力波 键盘3
-blackHole='4' #黑洞 键盘4
-dianxing=Button.left #电刑 鼠标左键
-yindao=Button.right  #引导 鼠标右键
+# 常量定义
+blackMan = '1'  # 黑人 键盘1
+meteorite = '2'  # 陨石 键盘2
+yuanlibo = '3'  # 原力波 键盘3
+blackHole = '4'  # 黑洞 键盘4
+dianxing = Button.left  # 电刑 鼠标左键
+yindao = Button.right  # 引导 鼠标右键
 
 
 # 宏循环的主体 具体的宏要放在这里去实现
@@ -22,22 +22,24 @@ class loopMain(object):
     # 鼠标抬起  mouse.release(Button.left)
 
     # keyWord 键盘点击 beginSec 按键开始的延迟时间 endSec 按键结束的延迟时间
-    def keyEnter(self,keyWord,sec):
+    def keyEnter(self, keyWord, sec):
         k.press_key(keyWord)
         time.sleep(sec)
         k.release_key(keyWord)
 
-    #满勾玉时的循环
-    def fullLoop(self,n):
-        while  n > 0:
+    # 满勾玉时的循环
+    def fullLoop(self, n):
+        num = 0
+        while n > num:
             self.loop_32()
-            n -= 1
-    
-    #0勾玉时的循环
-    def zeroLoop(self,n):
-        while  n > 0:
-            if n==30:
-                tt=time.time()
+            num += 1
+
+    # 0勾玉时的循环
+    def zeroLoop(self, n):
+        num = 0
+        while n > num:
+            if num == 0:
+                tt = time.time()
                 self.blackManLoop()
                 time.sleep(1.5)
                 self.meteorite_one()
@@ -49,12 +51,13 @@ class loopMain(object):
                 time.sleep(2.294)
                 mouse.release(dianxing)
                 self.meteorite_blackMan()
-                print("--",time.time()-tt)
+                print("0层勾玉第一次循环历时: ", time.time()-tt)
             else:
                 self.loop_32()
-            n -= 1
-    
+            num += 1
+    # 满勾玉 黑人起手 
     def loop_32(self):
+        tt = time.time()
         self.blackManLoop()
         time.sleep(1.5)
         self.meteorite_one()
@@ -62,18 +65,28 @@ class loopMain(object):
         time.sleep(2.094)
         mouse.release(dianxing)
         self.meteorite_blackMan()
+        print("32s带勾玉循环历时: ", time.time()-tt)
 
+    # 满勾玉 陨石起手 
+    def meteorite_fullLoop(self, n):
+        num = 0
+        while n > num:
+            if num == 0:
+                self.meteorite_blackMan()
+            self.loop_32()
+            num += 1
 
     # 单发陨石-带黑洞  黑洞-原力波-电刑-陨石-电刑-引导 持续时间4.255s
     def meteorite_one(self):
-        k.press_key(' ') #强制站立
-        k.press_key(blackHole) #按下黑洞
+        tt = time.time()
+        k.press_key(' ')  # 强制站立
+        k.press_key(blackHole)  # 按下黑洞
         k.release_key(blackHole)
         time.sleep(0.5)
         k.press_key(yuanlibo)
         k.release_key(yuanlibo)
         time.sleep(0.5)
-        #电刑5发 
+        # 电刑5发
         mouse.press(dianxing)
         time.sleep(1.75)
         mouse.release(dianxing)
@@ -87,29 +100,33 @@ class loopMain(object):
         mouse.press(yindao)
         time.sleep(0.4)
         mouse.release(yindao)
-        k.release_key(' ') #强制站立
+        k.release_key(' ')  # 强制站立
+        print("单发陨石历时: ", time.time()-tt)
 
-    #黑人20s循环 持续时间20.06s
+    # 黑人20s循环 持续时间20.06s
     def blackManLoop(self):
+        tt = time.time()
         k.press_key(blackMan)
         k.release_key(blackMan)
-        n=20
-        while n>0:
+        n = 20
+        while n > 0:
             k.press_key('1')
             k.release_key('1')
             time.sleep(1)
-            n-=1
-    
-    #黑人增伤的陨石 持续4.1267
+            n -= 1
+        print("黑人循环历时: ", time.time()-tt)
+
+    # 黑人增伤的陨石 持续4.1267
     def meteorite_blackMan(self):
-        k.press_key(' ') #强制站立
-        k.press_key(blackHole) #按下黑洞
+        tt = time.time()
+        k.press_key(' ')  # 强制站立
+        k.press_key(blackHole)  # 按下黑洞
         k.release_key(blackHole)
         time.sleep(0.5)
         k.press_key(yuanlibo)
         k.release_key(yuanlibo)
         time.sleep(0.5)
-        #电刑5发 
+        # 电刑5发
         mouse.press(dianxing)
         time.sleep(1.75)
         mouse.release(dianxing)
@@ -125,4 +142,5 @@ class loopMain(object):
         k.press_key(blackMan)
         k.release_key(blackMan)
         mouse.release(yindao)
-        k.release_key(' ') #强制站立
+        k.release_key(' ')  # 强制站立
+        print("双黑陨石历时: ", time.time()-tt)
